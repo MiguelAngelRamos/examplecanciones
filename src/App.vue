6c35961c2def18f002bcd51371397fd9d9f2f015
Miguel Angel Ramos <return.infox@gmail.com>
2022-03-21 19:27:04 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,52 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <h1 class="text-center mb-3">Listado de Canciones</h1>
+    <div class="row mt-5">
+      <div class="col-4">
+        <form @submit.prevent="submit">
+          <div class="mb-3">
+            <input class="form-control" type="text" v-model="artista"/>
+            <input class="form-control" type="text" v-model="cancion"/>
+          </div>
+          <button class="btn btn-success">Buscar Canción</button>
+        </form>
+      </div>
+      <div class="col-8">
+        <p>Nombre Artista: {{artista}}</p>
+        <p>Cancion: {{cancion}}</p>
+        <p>letra de la cancion</p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { consumirCanciones } from "./api";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  components: {},
+  data() {
+    return {
+      artista: "",
+      cancion: "",
+    };
+  },
+  methods: {
+    async submit() {
+      console.log('Se ejecuta el método submit')
+
+      // 1 ejemplo
+      //consumirCanciones(this.artista, this.cancion).then( letra => console.log(letra.lyrics));
+
+      // 2 Ejemplo
+
+      const letraCancion = await consumirCanciones(this.artista, this.cancion);
+      console.log(letraCancion);
+    }
   }
-}
+
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
